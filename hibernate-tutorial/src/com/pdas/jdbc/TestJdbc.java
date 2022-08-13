51652b6ae06676c65d227fb8613e9153ba3754f8
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-13 15:04:29 +0530

    Section 19 completed

---
+++ hibernate-tutorial/src/com/pdas/jdbc/TestJdbc.java
@@ -0,0 +1,21 @@
+package com.pdas.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+	public static void main(String[] args) {
+		String jdbcURL = "jdbc:mysql://127.0.0.1:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		try {
+			System.out.println("Connecting to database: " + jdbcURL);
+			Connection connection = DriverManager.getConnection(jdbcURL, user, password);
+			System.out.println("Connection Successful!");
+		} catch(Exception exception) {
+			exception.printStackTrace();
+		}
+	}
+
+}
