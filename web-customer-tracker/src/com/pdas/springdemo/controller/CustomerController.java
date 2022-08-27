39f14f83bbb559559e94284ba97d361ba66b7f83
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-27 09:11:12 +0530

    Section 30 completed

--- web-customer-tracker/src/com/pdas/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/pdas/springdemo/controller/CustomerController.java
@@ -5,24 +5,26 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pdas.springdemo.dao.CustomerDAO;
 import com.pdas.springdemo.entity.Customer;
+import com.pdas.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
-	// need to inject the customer dao
+	// need to inject the customer service
 	@Autowired
-	public CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
-		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		// add the customers to the model
 		model.addAttribute("customers", theCustomers);
---
+++ web-customer-tracker/src/com/pdas/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.pdas.springdemo.service;
+
+import java.util.List;
+
+import com.pdas.springdemo.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+	
+}
