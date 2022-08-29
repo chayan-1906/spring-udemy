d7867886d9d560a15f46e1148aea617daaffcf0a
Padmanabha Das <padmanabhadas9647@gmail.com>
2022-08-29 23:06:28 +0530

    Completed upto Section 35

---
+++ spring-demo-aop/src/com/pdas/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.pdas.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.pdas.aopdemo")
+public class DemoConfig {
+
+}
---
+++ spring-demo-aop/src/com/pdas/aopdemo/MainDemoApp.java
@@ -0,0 +1,31 @@
+package com.pdas.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.pdas.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+		// get the bean from spring container
+		AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		// call the business method
+		accountDAO.addAccount();
+
+		// do it again!
+		System.out.println("\n let's call it again!\n");
+
+		// call the business method again
+		accountDAO.addAccount();
+
+		// close the context
+		context.close();
+
+	}
+
+}
