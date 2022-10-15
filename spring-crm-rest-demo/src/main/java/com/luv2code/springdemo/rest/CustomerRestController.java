3224f5830b6cec8c812925e147580300e5aa1e77
pdas9647 <padmanabhadas9647@gmail.com>
2022-10-16 00:46:23 +0530

    Section 67 completed

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,13 @@ public class CustomerRestController {
 	public Customer addCustomer(@RequestBody Customer customer) {
 		// this is force a save of new item... instead of update
 		customer.setId(0);
+		customerService.saveCustomer(customer);
+		return customer;
+	}
+
+	// add mapping for PUT /customer - update existing customer
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer customer) {
 		customerService.saveCustomer(customer);
 		return customer;
 	}
